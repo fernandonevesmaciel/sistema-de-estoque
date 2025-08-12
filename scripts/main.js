8a68eb319a909dbc4ac800d3fde0ba177e69a7b2
Fernando Maciel <fernandonevesmaciel@gmail.com>
2025-08-12 20:31:18 -0300

    Configuração inicial

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// O console.log é uma forma de "imprimir" algo no console do navegador.
+// É ótimo para testar e depurar nosso código.
+console.log("Olá do JavaScript! O script foi carregado com sucesso.");
+
+// Podemos adicionar uma interação simples.
+// Quando a página carregar, um alerta será exibido.
+window.onload = function() {
+    alert("O site foi carregado!");
+};
